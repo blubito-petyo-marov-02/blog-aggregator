365aa8175af6a74ffc720a74148fbbc00b08d897
[Blubito] Petyo Marov <p.marov@blubito.com>
2026-03-08 11:42:47 +0200

    feat: posts

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -28,3 +28,18 @@ export const feedFollows = pgTable('feed_follows', {
   userId: uuid('user_id').references(() => users.id, { onDelete: 'cascade' }).notNull(),
   feedId: uuid('feed_id').references(() => feeds.id, { onDelete: 'cascade'}).notNull(),
 }, (t) => ({ unq: unique().on(t.userId, t.feedId)}));
+
+export type FeedFollow = typeof feedFollows.$inferSelect;
+
+export const posts = pgTable('posts', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow().$onUpdate(() => new Date()),
+  title: text('title').notNull(),
+  url: text('url').notNull().unique(),
+  description: text('description'),
+  publishedAt: timestamp('published_at'),
+  feedId: uuid('feed_id').references(() => feeds.id, { onDelete: 'cascade'}).notNull(),
+});
+
+export type Post = typeof posts.$inferSelect;
